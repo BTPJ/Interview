3368cc0ca9e4a8d5054e4c2d205bf7226aeff1f4
BTPJ <1069113473@qq.com>
2021-11-29 17:11:21 +0800

    添加策略模式

---
+++ DesignPattern/src/main/java/com/btpj/design_pattern/strategy/CalculateStrategy.java
@@ -0,0 +1,17 @@
+package com.btpj.design_pattern.strategy;
+
+/**
+ * 定义计算策略
+ *
+ * @author LTP  2021/11/29
+ */
+public interface CalculateStrategy {
+
+    /**
+     * 抽象计算方法
+     *
+     * @param km 距离
+     * @return 根据距离计算出的费用
+     */
+    int calculate(int km);
+}
