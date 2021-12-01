3919e4c3aff9f2fa727d05cd6447318969878861
BTPJ <1069113473@qq.com>
2021-12-01 14:54:33 +0800

    添加多线程项目

---
+++ JavaOrKotlin/MultiThread/src/main/java/com/btpj/multithread/AtomicDemo.java
@@ -0,0 +1,62 @@
+package com.btpj.multithread;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * java.util.concurrent下面的原子操作，包括AtomicInteger、AtomicLong、AtomicBoolean等
+ * 采用CAS原子组件的方式进行同步
+ * CAS存在的问题：
+ * 1.CPU占用高，高并发不适合
+ * 2.复杂操作不适合
+ * 3.无法避免ABA问题
+ *
+ * @author LTP 2021/12/1
+ */
+public class AtomicDemo {
+    static final AtomicDemo mAtomicDemo = new AtomicDemo();
+
+    //使用atomicInteger包装后进行原子操作能使得线程同步
+    private static final AtomicInteger atomicInteger = new AtomicInteger(1000000000);
+
+    /**
+     * 每次减一
+     */
+    private void reduce() {
+        atomicInteger.decrementAndGet();
+    }
+
+    /**
+     * 执行
+     */
+    public void execute() {
+        // 普通方式开启1000个线程执行
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < 1000; i++) {
+            new Thread(new MyRunnable()).start();
+        }
+        while (true) {
+            if (Thread.activeCount() == 1) {
+                System.out.println("普通方式执行线程加Atomic方式完毕，结果为" + atomicInteger + "，耗时" + (System.currentTimeMillis() - startTime) + "ms");
+                break;
+            }
+        }
+    }
+
+
+    static class MyRunnable implements Runnable {
+
+        @Override
+        public void run() {
+
+            // 子线程中做10000次reduce操作
+            for (int i = 0; i < 1000000; i++) {
+                mAtomicDemo.reduce();
+            }
+
+        }
+    }
+
+    public static void main(String[] args) {
+        mAtomicDemo.execute(); // 21s左右
+    }
+}
---
+++ JavaOrKotlin/MultiThread/src/main/java/com/btpj/multithread/ExecutorDemo.java
@@ -0,0 +1,59 @@
+package com.btpj.multithread;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * 采用线程池 + Synchronized关键字
+ *
+ * @author LTP 2021/12/1
+ */
+public class ExecutorDemo {
+    static final ExecutorDemo mExecutorDemo = new ExecutorDemo();
+    private static int mNum = 1000000000;
+
+    /**
+     * 采用类锁的方式加锁，普通方法锁的是对象，当多个对象时无用,这里由于只有一个SynchronizedDemo对象，故都可以
+     */
+    private void reduce() {
+        synchronized (ExecutorDemo.class) {
+            mNum--;
+        }
+    }
+
+    /**
+     * 执行
+     */
+    public void execute() {
+        // 使用线程池开启1000个线程执行
+        long startTime = System.currentTimeMillis();
+        ExecutorService executorService = Executors.newFixedThreadPool(50);
+        for (int i = 0; i < 1000; i++) {
+            executorService.execute(new MyRunnable());
+        }
+        executorService.shutdown();
+        while (true) {
+            //  if (Thread.activeCount() == 1) {
+            if (executorService.isTerminated()) {
+                System.out.println("线程池方式执行线程加Synchronized锁完毕，结果为" + mNum + "，耗时" + (System.currentTimeMillis() - startTime) + "ms");
+                break;
+            }
+        }
+    }
+
+    static class MyRunnable implements Runnable {
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 1000000; i++) {
+                mExecutorDemo.reduce();
+            }
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+        mExecutorDemo.execute(); // 13s左右
+    }
+}
