4b7c1bbfbbd4585d9a80e692549421739552980b
BTPJ <1069113473@qq.com>
2021-12-07 13:31:06 +0800

    完成Map接口下的集合类

--- JavaOrKotlin/Collections/src/main/java/com/btpj/collection/map/TreeMapDemo.java
+++ JavaOrKotlin/Collections/src/main/java/com/btpj/collection/map/TreeMapDemo.java
@@ -19,12 +19,7 @@ public class TreeMapDemo {
         treeMap.put(3, "LTP3");
         System.out.println(treeMap);
 
-        TreeMap<Integer, String> treeMap2 = new TreeMap<>(new Comparator<Integer>() {
-            @Override
-            public int compare(Integer t1, Integer t2) {
-                return t2 - t1;
-            }
-        });
+        TreeMap<Integer, String> treeMap2 = new TreeMap<>((t1, t2) -> t2 - t1);
         treeMap2.put(1, "LTP1");
         treeMap2.put(2, null);
         treeMap2.put(4, null);
