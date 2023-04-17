02a4f20530e39f226b57975ebcd67bb5f778b07c
LTP <1069113473@qq.com>
2023-04-17 10:12:13 +0800

    完成部分动态规划算法

--- Algorithm/src/array/MaxSubArray.java
+++ Algorithm/src/array/MaxSubArray.java
@@ -29,4 +29,14 @@ public class MaxSubArray {
         }
         return maxSum;
     }
+
+    private static int maxSubArray2(int[] nums) {
+        int result = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] += Math.max(nums[i - 1], 0);
+            result = Math.max(result, nums[i]);
+        }
+
+        return result;
+    }
 }
