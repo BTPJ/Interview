bc456047064d618d0ad197c87a19d993d99b09a8
LTP <1069113473@qq.com>
2023-05-26 11:37:53 +0800

    代码优化

--- KotlinStudy/src/main/java/com/btpj/coroutines/CoroutinesConcurrent.kt
+++ KotlinStudy/src/main/java/com/btpj/coroutines/CoroutinesConcurrent.kt
@@ -2,125 +2,114 @@ package com.btpj.coroutines
 
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
 import kotlinx.coroutines.ObsoleteCoroutinesApi
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.channels.actor
+import kotlinx.coroutines.joinAll
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.Semaphore
 import kotlinx.coroutines.sync.withLock
 import kotlinx.coroutines.sync.withPermit
+import org.junit.Test
 import java.util.concurrent.atomic.AtomicInteger
 import java.util.concurrent.locks.ReentrantLock
-import kotlin.system.measureTimeMillis
 
 /**
  * 协程的几种并发安全方案对比
- * AtomicInteger（原子操作）> ReentrantLock（可重入锁） > synchronized（线程同步锁）
- * > Semaphore（协程信号量限制） > actor通信并发同步模型 > Mutex（协程互斥锁）
+ * 非同步 > AtomicInteger（原子操作）> synchronized（线程同步锁）> ReentrantLock（可重入锁）
+ * > Semaphore（协程信号量限制） >  Mutex（协程互斥锁）> actor通信并发同步模型
  *
  * @author LTP  2023/5/25
  */
-object CoroutinesConcurrent {
+class CoroutinesConcurrent {
 
-    @JvmStatic
-    fun main(args: Array<String>) {
-        println("普通方式总耗时：${measureTimeMillis { testSimple() }}")
-        println("----------------------------------------------------")
-
-        println("Mutex协程锁同步方式总耗时：${measureTimeMillis { testMutex() }}")
-        println("----------------------------------------------------")
-
-        println("synchronized线程锁同步方式总耗时：${measureTimeMillis { testSynchronized() }}")
-        println("----------------------------------------------------")
-
-        println("ReentrantLock线程锁同步方式总耗时：${measureTimeMillis { testReentrantLock() }}")
-        println("----------------------------------------------------")
-
-        println("Atomic原子操作实现同步方式总耗时：${measureTimeMillis { testAtomic() }}")
-        println("----------------------------------------------------")
-
-        println("Semaphore协程信号量实现同步方式总耗时：${measureTimeMillis { testSemaphore() }}")
-        println("----------------------------------------------------")
-
-        println("actor实现同步方式总耗时：${measureTimeMillis { testActor() }}")
-    }
-
-    /** 使用非同步方式 */
-    private fun testSimple() {
-        runBlocking {
-            var count = 0
-            val list = ArrayList<Job>()
-
-            repeat(100) {
-                list.add(
-                    CoroutineScope(Dispatchers.IO).launch {
-                        repeat(1000) {
-                            count++
-                        }
-                    })
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            CoroutinesConcurrent().apply {
+                testSimple()
+                testAtomic()
+                testSynchronized()
+                testReentrantLock()
+                testSemaphore()
+                testMutex()
+                testActor()
             }
-
-            list.forEach { it.join() }
-
-            println("count值为：${count}")
         }
     }
 
-    /** 使用Mutex协程锁实现同步 */
-    private fun testMutex() {
+    /** 使用非同步方式 47ms */
+    @Test
+    fun testSimple() {
         runBlocking {
+            val start = System.currentTimeMillis()
+            var count = 0
+
+            List(1000) {
+                CoroutineScope(Dispatchers.Default).launch {
+                    repeat(10000) {
+                        count++
+                    }
+                }
+            }.joinAll()
+
+            println("count值为：${count}，非同步耗时为：${System.currentTimeMillis() - start}")
+        }
+    }
+
+    /** 使用Mutex协程锁实现同步 14881ms */
+    @Test
+    fun testMutex() {
+        runBlocking {
+            val start = System.currentTimeMillis()
             var count = 0
             val mutex = Mutex()
-            val list = ArrayList<Job>()
 
-            repeat(100) {
-                list.add(
-                    CoroutineScope(Dispatchers.IO).launch {
-                        repeat(1000) {
-                            mutex.withLock {
-                                count++
-                            }
+            List(1000) {
+                CoroutineScope(Dispatchers.Default).launch {
+                    repeat(10000) {
+                        mutex.withLock {
+                            count++
                         }
-                    })
-            }
+                    }
 
-            list.forEach { it.join() }
+                }
+            }.joinAll()
 
-            println("count值为：${count}")
+            println("count值为：${count}，Mutex耗时为：${System.currentTimeMillis() - start}")
         }
     }
 
-    /** 使用Semaphore协程信号量实现同步 */
-    private fun testSemaphore() {
+    /** 使用Semaphore协程信号量实现同步 1518ms */
+    @Test
+    fun testSemaphore() {
         runBlocking {
+            val start = System.currentTimeMillis()
             var count = 0
             val semaphore = Semaphore(1)
-            val list = ArrayList<Job>()
 
-            repeat(100) {
-                list.add(
-                    CoroutineScope(Dispatchers.IO).launch {
-                        repeat(1000) {
-                            semaphore.withPermit {
-                                count++
-                            }
+            List(1000) {
+                CoroutineScope(Dispatchers.Default).launch {
+                    repeat(10000) {
+                        semaphore.withPermit {
+                            count++
                         }
-                    })
-            }
+                    }
+                }
+            }.joinAll()
 
-            list.forEach { it.join() }
-
-            println("count值为：${count}")
+            println("count值为：${count}，Semaphore耗时为：${System.currentTimeMillis() - start}")
         }
     }
 
-    /** 使用actor并发同步模型实现同步 */
+    /** 使用actor并发同步模型实现同步 4497ms */
     @OptIn(ObsoleteCoroutinesApi::class)
-    private fun testActor() {
+    @Test
+    fun testActor() {
         runBlocking {
+            val start = System.currentTimeMillis()
             var count = 0
 
             val actor = actor<Int>(capacity = Channel.UNLIMITED) {
@@ -129,90 +118,80 @@ object CoroutinesConcurrent {
                 }
             }
 
-            val list = ArrayList<Job>()
-
-            repeat(100) {
-                list.add(
-                    CoroutineScope(Dispatchers.IO).launch {
-                        repeat(1000) {
-                            actor.send(1)
-                        }
-                    })
-            }
-
-            list.forEach { it.join() }
+            List(1000) {
+                CoroutineScope(Dispatchers.Default).launch {
+                    repeat(10000) {
+                        actor.send(1)
+                    }
+                }
+            }.joinAll()
 
             actor.close()
 
-            println("count值为：${count}")
+            println("count值为：${count}，Actor耗时为：${System.currentTimeMillis() - start}")
         }
     }
 
-    /** 使用synchronized线程锁实现同步 */
-    private fun testSynchronized() {
+    /** 使用synchronized线程锁实现同步 106ms */
+    @Test
+    fun testSynchronized() {
         runBlocking {
+            val start = System.currentTimeMillis()
             var count = 0
-            val list = ArrayList<Job>()
+            val name = "123"
 
-            repeat(100) {
-                list.add(
-                    CoroutineScope(Dispatchers.IO).launch {
-                        repeat(1000) {
-                            synchronized(list) {
-                                count++
-                            }
+            List(1000) {
+                CoroutineScope(Dispatchers.Default).launch {
+                    repeat(10000) {
+                        synchronized(name) {
+                            count++
                         }
-                    })
-            }
+                    }
+                }
+            }.joinAll()
 
-            list.forEach { it.join() }
-
-            println("count值为：${count}")
+            println("count值为：${count}，Synchronized耗时为：${System.currentTimeMillis() - start}")
         }
     }
 
-    /** 使用ReentrantLock线程锁实现同步 */
-    private fun testReentrantLock() {
+    /** 使用ReentrantLock线程锁实现同步 241ms*/
+    @Test
+    fun testReentrantLock() {
         runBlocking {
+            val start = System.currentTimeMillis()
             var count = 0
             val lock = ReentrantLock()
-            val list = ArrayList<Job>()
 
-            repeat(100) {
-                list.add(
-                    CoroutineScope(Dispatchers.IO).launch {
-                        repeat(1000) {
-                            lock.lock()
-                            count++
-                            lock.unlock()
-                        }
-                    })
-            }
+            List(1000) {
+                CoroutineScope(Dispatchers.Default).launch(Dispatchers.Default) {
+                    repeat(10000) {
+                        lock.lock()
+                        count++
+                        lock.unlock()
+                    }
+                }
+            }.joinAll()
 
-            list.forEach { it.join() }
-
-            println("count值为：${count}")
+            println("count值为：${count}，ReentrantLock耗时为：${System.currentTimeMillis() - start}")
         }
     }
 
-    /** 使用Atomic原子操作实现同步 */
-    private fun testAtomic() {
+    /** 使用Atomic原子操作实现同步 140ms */
+    @Test
+    fun testAtomic() {
         runBlocking {
+            val start = System.currentTimeMillis()
             val atomicCount = AtomicInteger(0)
-            val list = ArrayList<Job>()
 
-            repeat(100) {
-                list.add(
-                    CoroutineScope(Dispatchers.IO).launch {
-                        repeat(1000) {
-                            atomicCount.incrementAndGet()
-                        }
-                    })
-            }
+            List(1000) {
+                CoroutineScope(Dispatchers.Default).launch(Dispatchers.Default) {
+                    repeat(10000) {
+                        atomicCount.incrementAndGet()
+                    }
+                }
+            }.joinAll()
 
-            list.forEach { it.join() }
-
-            println("count值为：${atomicCount.get()}")
+            println("count值为：${atomicCount.get()}，Atomic原子操作耗时为：${System.currentTimeMillis() - start}")
         }
     }
 }
